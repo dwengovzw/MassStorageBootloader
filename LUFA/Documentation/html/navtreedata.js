609eae9ec9ceb7f48430e67da3ff09ff3a1311fe
Tom Neutens <tomneutens@gmail.com>
2021-08-18 16:52:28 +0200

    added LUFA library to repo since I made some modifications to the source code, it does not work with the original library

---
+++ LUFA/Documentation/html/navtreedata.js
@@ -0,0 +1,137 @@
+var NAVTREE =
+[
+  [ "LUFA Library", "index.html", [
+    [ "Source Code License", "_page__license_info.html", [
+      [ "License Summary for Human Beings", "_page__license_info.html#Sec_LicenseForHumans", null ],
+      [ "Commercial Licensing", "_page__license_info.html#Sec_CommercialLicenses", null ],
+      [ "LUFA License Text", "_page__license_info.html#Sec_LicenseText", null ]
+    ] ],
+    [ "Donating to Support This Project", "_page__donating.html", null ],
+    [ "Device and Hardware Support", "_page__device_support.html", "_page__device_support" ],
+    [ "Project Changelog", "_page__change_log.html", [
+      [ "Version XXXXXX", "_page__change_log.html#Sec_ChangeLogXXXXXX", null ],
+      [ "Version 210130", "_page__change_log.html#Sec_ChangeLog210130", null ],
+      [ "Version 170418", "_page__change_log.html#Sec_ChangeLog170418", null ],
+      [ "Version 151115", "_page__change_log.html#Sec_ChangeLog151115", null ],
+      [ "Version 140928", "_page__change_log.html#Sec_ChangeLog140928", null ],
+      [ "Version 140302", "_page__change_log.html#Sec_ChangeLog140302", null ],
+      [ "Version 130901", "_page__change_log.html#Sec_ChangeLog130901", null ],
+      [ "Version 130303", "_page__change_log.html#Sec_ChangeLog130303", null ],
+      [ "Version 120730", "_page__change_log.html#Sec_ChangeLog120730", null ],
+      [ "Version 120219", "_page__change_log.html#Sec_ChangeLog120219", null ],
+      [ "Version 111009", "_page__change_log.html#Sec_ChangeLog111009", null ],
+      [ "Version 110528", "_page__change_log.html#Sec_ChangeLog110528", null ],
+      [ "Version 101122", "_page__change_log.html#Sec_ChangeLog101122", null ],
+      [ "Version 100807", "_page__change_log.html#Sec_ChangeLog100807", null ],
+      [ "Version 100513", "_page__change_log.html#Sec_ChangeLog100513", null ],
+      [ "Version 100219", "_page__change_log.html#Sec_ChangeLog100219", null ],
+      [ "Version 091223", "_page__change_log.html#Sec_ChangeLog091223", null ],
+      [ "Version 091122", "_page__change_log.html#Sec_ChangeLog091122", null ],
+      [ "Version 090924", "_page__change_log.html#Sec_ChangeLog090924", null ],
+      [ "Version 090810", "_page__change_log.html#Sec_ChangeLog090810", null ],
+      [ "Version 090605", "_page__change_log.html#Sec_ChangeLog090605", null ],
+      [ "Version 090510", "_page__change_log.html#Sec_ChangeLog090510", null ],
+      [ "Version 090401", "_page__change_log.html#Sec_ChangeLog090401", null ],
+      [ "Version 090209", "_page__change_log.html#Sec_ChangeLog090209", null ],
+      [ "Version 081224", "_page__change_log.html#Sec_ChangeLog081224", null ],
+      [ "Version 1.5.3 (081002)", "_page__change_log.html#Sec_ChangeLog153", null ],
+      [ "Version 1.5.2 (080731)", "_page__change_log.html#Sec_ChangeLog152", null ],
+      [ "Version 1.5.1 (080707)", "_page__change_log.html#Sec_ChangeLog151", null ],
+      [ "Version 1.5.0 (080610)", "_page__change_log.html#Sec_ChangeLog150", null ],
+      [ "Version 1.4.1 (090519)", "_page__change_log.html#Sec_ChangeLog141", null ],
+      [ "Version 1.4.0 (090505)", "_page__change_log.html#Sec_ChangeLog140", null ],
+      [ "Version 1.3.2 (080401)", "_page__change_log.html#Sec_ChangeLog132", null ],
+      [ "Version 1.3.1 (080319)", "_page__change_log.html#Sec_ChangeLog131", null ],
+      [ "Version 1.3.0 (080307)", "_page__change_log.html#Sec_ChangeLog130", null ],
+      [ "Version 1.2.0 (080204)", "_page__change_log.html#Sec_ChangeLog120", null ],
+      [ "Version 1.1.0 (080125)", "_page__change_log.html#Sec_ChangeLog110", null ]
+    ] ],
+    [ "Known Issues", "_page__known_issues.html", [
+      [ "Version XXXXXX", "_page__known_issues.html#Sec_KnownIssuesXXXXXX", null ],
+      [ "Version 210130", "_page__known_issues.html#Sec_KnownIssues210130", null ],
+      [ "Version 170418", "_page__known_issues.html#Sec_KnownIssues170418", null ],
+      [ "Version 151115", "_page__known_issues.html#Sec_KnownIssues151115", null ],
+      [ "Version 140928", "_page__known_issues.html#Sec_KnownIssues140928", null ],
+      [ "Version 140302", "_page__known_issues.html#Sec_KnownIssues140302", null ],
+      [ "Version 130901", "_page__known_issues.html#Sec_KnownIssues130901", null ],
+      [ "Version 130303", "_page__known_issues.html#Sec_KnownIssues130303", null ],
+      [ "Version 120730", "_page__known_issues.html#Sec_KnownIssues120730", null ]
+    ] ],
+    [ "Future Changes", "_page__future_changes.html", null ],
+    [ "Getting Started", "_page__getting_started.html", "_page__getting_started" ],
+    [ "Developing With LUFA", "_page__developing_with_l_u_f_a.html", "_page__developing_with_l_u_f_a" ],
+    [ "User Projects Powered by LUFA", "_page__l_u_f_a_powered_projects.html", [
+      [ "AVR-USB Development Boards Using LUFA", "_page__l_u_f_a_powered_projects.html#Sec_BoardsUsingLUFA", null ],
+      [ "Projects Using LUFA (Hobbyist)", "_page__l_u_f_a_powered_projects.html#Sec_LUFAProjects", null ],
+      [ "Projects Using LUFA (Commercial)", "_page__l_u_f_a_powered_projects.html#Sec_LUFACommercialProjects", null ],
+      [ "Publications Mentioning LUFA", "_page__l_u_f_a_powered_projects.html#Sec_LUFAPublications", null ],
+      [ "Other Notable Mentions of LUFA", "_page__l_u_f_a_powered_projects.html#Sec_LUFANotableMentions", null ],
+      [ "Non-Official LUFA Ports and Forks", "_page__l_u_f_a_powered_projects.html#Sec_PortsAndForks", null ]
+    ] ],
+    [ "Library Resources", "_page__resources.html", [
+      [ "Unofficial Resources", "_page__resources.html#Sec_UnofficialResources", null ],
+      [ "LUFA Related Webpages", "_page__resources.html#Sec_ProjectPages", null ],
+      [ "Assistance With LUFA", "_page__resources.html#Sec_ProjectHelp", null ],
+      [ "Latest In-Development Source Code", "_page__resources.html#Sec_InDevelopment", null ],
+      [ "USB Resources", "_page__resources.html#Sec_USBResources", null ]
+    ] ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_a_d_c_8h.html",
+"_page__build_module__l_u_f_a__s_o_u_r_c_e_s.html#SSec_BuildModule_LUFA_SOURCES_ProvidedVariables",
+"dir_7d64b2eb7704564c22b62dc99b49a048.html",
+"group___group___a_t45_d_b321_c.html#ga10d1fc6b19e95d886bd3be33a2a92000",
+"group___group___board_info___e_v_k1104.html#ga533c886382f90af8e9be1f3b95ae52fb",
+"group___group___board_types.html#gabde4cc3131d55de8dcfc4153f48176d5",
+"group___group___config_descriptor_parser.html#gga260aca2e8a4697e42cd0395eebfd2dbca672aa29f1dc19197700d933412d94845",
+"group___group___dataflash___x_p_l_a_i_n.html#ga5a83f6d2e43ada7761beba410b329439",
+"group___group___endpoint_packet_management___a_v_r8.html#ga4fe130e3ce8121c25bba1dd0668bca32",
+"group___group___endpoint_r_w___x_m_e_g_a.html#ggaa42b7eb8d1be3afadb97097bf2605740ac395cfec6ba73cd76fd016e746b7dc74",
+"group___group___h_i_d_parser.html#ggaecb44fa2b3386dc8c2a07acbd26e1569a0de8cb679854f9b333b1962344b9027b",
+"group___group___joystick.html#gac212d43aba13b48eca512e1eb49ceac0",
+"group___group___l_e_ds___e_v_k1101.html#ga067978238d36887c2c33d74e69f90bce",
+"group___group___l_e_ds___u_c3_a3___x_p_l_a_i_n_e_d.html#gab03d373544b1782a0dcb43968fe84db9",
+"group___group___pipe_management___u_c3.html#ga263a33a811ddf056249e4e043435ac10",
+"group___group___pipe_stream_r_w___a_v_r8.html#gaedb8cea65a5e02ac5464cc9bfab66e58",
+"group___group___s_p_i___x_m_e_g_a.html#gadfb25a268f39349e5432712199121399",
+"group___group___t_w_i___a_v_r8.html#ga215532624ce3ebbe9298dbb8a2b77b14",
+"group___group___u_s_b_class_android_accessory_host.html#gabb51bd34b675f99cb0dbffb2efe61635",
+"group___group___u_s_b_class_audio_host.html#ggacfc3536f2850bd8b978fed648fb409dbaf01a6212749949ce763bf804189e0c6d",
+"group___group___u_s_b_class_c_d_c_common.html#gga616d151903098f35bb9191cc90b99b85a5c5870f8c88a8d51db6801744aa19504",
+"group___group___u_s_b_class_h_i_d_common.html#ga4d08ea3d4d390aa15410e9c2220e4a77",
+"group___group___u_s_b_class_h_i_d_common.html#gacc0a3a0d29e75ab5b63ce8c5eed49fc4",
+"group___group___u_s_b_class_m_i_d_i_device.html#gaeb0a51c4524af9a331599a2a520326db",
+"group___group___u_s_b_class_printer_host.html#gab463d5d517c4426098f9135c74f70e66",
+"group___group___u_s_b_class_s_i_common.html#ggab7e935c7a8fb319b95f8498f252ffa19a424cb97a1a522e02e09ee0975ed45434",
+"struct_h_i_d___report_item__t.html#a78c896bccb1f711623aba2f77294a793",
+"struct_u_s_b___audio___std_descriptor___input_terminal__t.html#a4167852fc8d671751a445a932e2627d5",
+"struct_u_s_b___class_info___m_s___device__t.html#a4a75c2e3bc2ea4514128eb91ed26cb68",
+"struct_u_s_b___std_descriptor___device_qualifier__t.html#a7876d665933e8ab19d7c51e85a844e6b"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
